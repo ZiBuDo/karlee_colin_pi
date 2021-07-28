d49768667fbfa04b967bf391a9ee18b3158a8a2f
Paul <pgregotski@gmail.com>
2021-07-28 19:45:06 -0400

    Test pi

--- program.js
+++ program.js
@@ -37,34 +37,33 @@ const sprites = {
                 width: 64,
                 height: 64,
                 frames: [1, 5, 9, 13],
-                scale: 4,
-                offset: 24
+                scale: 1
             },
             gray: {
                 height: 117,
                 width: 165,
                 frames: [0, 1, 2],
-                scale: 1,
+                scale: 1/3,
                 offset: -32
             },
             nyan: {
                 height: 20,
                 width: 58.8,
                 frames: [0, 1, 2, 3, 4],
-                scale: 4,
+                scale: 1,
                 offset: -32
             },
             sleeping: {
                 height: 160,
                 width: 192.5,
                 frames: [0, 1, 2, 3],
-                scale: 1,
-                offset: -32
+                scale: 1/3,
+                offset: -24
             },
             tiger: {
                 height: 56,
                 width: 56,
-                scale: 4
+                scale: 1
             }
         },
         sprite: null,
@@ -95,6 +94,11 @@ const sprites = {
     }
 };
 
+const defaultScale = 2;
+const defaultHeight = 48;
+const defaultWidth = 32;
+const defaultOffset = -16;
+
 $(function () {
     new Phaser.Game({
         width: "100%",
@@ -119,8 +123,8 @@ function preload() {
         Object.keys(sprites[key].skins).forEach(k => {
             const skin = sprites[key].skins[k];
             scene.load.spritesheet(`${key}-${k}`, `/journey/sprites/${key}/${key}_${k}.png`, {
-                frameWidth: skin.width ? skin.width : 32,
-                frameHeight: skin.height ? skin.height : 48
+                frameWidth: skin.width ? skin.width : defaultWidth,
+                frameHeight: skin.height ? skin.height : defaultHeight
             });
         });
     });
@@ -158,8 +162,8 @@ function create() {
         function addSprite() {
             const skin = config.skins[skins[config.skin]];
             config.sprite = scene.add.sprite(positions[config.position] * window.innerWidth,
-                    ((skin.offset ? skin.offset : 0) + window.innerHeight) - ((skin.height ? skin.height : 48) * (skin.scale ? skin.scale : 6)) / 2, `${key}-${skins[config.skin]}`).play(`walk-${key}-${skins[config.skin]}`)
-                .setScale(skin.scale ? skin.scale : 6)
+                    ((skin.offset ? skin.offset : defaultOffset) + window.innerHeight) - ((skin.height ? skin.height : defaultHeight) * (skin.scale ? skin.scale : 6)) / 2, `${key}-${skins[config.skin]}`).play(`walk-${key}-${skins[config.skin]}`)
+                .setScale(skin.scale ? skin.scale : defaultScale)
                 .setInteractive({
                     draggable: true
                 }).on("pointerup", function () {
@@ -224,8 +228,8 @@ function create() {
                 const skins = Object.keys(config.skins);
                 const skin = config.skins[skins[config.skin]];
                 const diff = (window.innerWidth * 1.3 / 2) - (window.innerWidth / 2);
-                config.sprite.setPosition((positions[config.position] * window.innerWidth) + (diff * progress), 
-                    ((skin.offset ? skin.offset : 0) + window.innerHeight) - ((skin.height ? skin.height : 48) * (skin.scale ? skin.scale : 6)) / 2);
+                config.sprite.setPosition((positions[config.position] * window.innerWidth) + (diff * progress),
+                    ((skin.offset ? skin.offset : defaultOffset) + window.innerHeight) - ((skin.height ? skin.height : defaultHeight) * (skin.scale ? skin.scale : defaultScale)) / 2);
             });
 
             if (progress === 1) {
@@ -236,6 +240,13 @@ function create() {
                 camera.setPosition(0, 0);
                 camera.centerOn(window.innerWidth / 2, window.innerHeight / 2);
                 camera.setScroll(0, 0);
+                Object.keys(sprites).forEach(key => {
+                    const config = sprites[key];
+                    const skins = Object.keys(config.skins);
+                    const skin = config.skins[skins[config.skin]];
+                    config.sprite.setPosition((positions[config.position] * window.innerWidth),
+                        ((skin.offset ? skin.offset : defaultOffset) + window.innerHeight) - ((skin.height ? skin.height : defaultHeight) * (skin.scale ? skin.scale : defaultScale)) / 2);
+                });
                 setTimeout(() => {
                     bg.destroy(true);
                     addBackground();
